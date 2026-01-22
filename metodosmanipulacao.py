67e2b55b86b63bc7a5e47863a0538693481f6eab
Bruno David Martins <dbrunobruno369@gmail.com>
2026-01-22 00:29:52 -0300

    Metodos de manipulação de conjuntos

--- metodosmanipulacao.py
+++ metodosmanipulacao.py
@@ -19,3 +19,25 @@ a-b                # ex: difference(set2) ou (-)
 
 a.symmetric_difference(b)    # Diferença entre dois conjuntos, considerando elementos exclusivos de ambos conjuntos.
 a^b                          # symmetric_difference(set2) ou (^)
+
+""" 
+Podemos analisar a relação entre conjuntos e suas características utilizando alguns métodos e criar cópias de conjuntos.
+"""
+
+# Considere:
+a = {1, 2}
+b = {1, 2, 3, 4}
+
+a.issubset(b)    # Verifica se um conjunto é subconjunto de outro.
+a<=b             # ex: issubset(set2) ou (<=)
+
+a.issuperset(b)    # Verifica se um conjunto é superconjunto de outro.
+a>=b               # ex: issuperset(set2) ou (>=)
+
+c = {5, 6, 7}    # Verfica se ps conjuntos não tem elementos em comum
+a.isdisjoint(c)  # ex: isdisjoin(set2)
+
+len(a)    # Verifica o número de elementos de um conjunto. ex: len(conjunto)
+
+copia = a.copy()    # Retorna uma cópia do conjunto.
+print(copia)        # ex: copy()
