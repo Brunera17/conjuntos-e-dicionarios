fab309c2ba8c607d1d70424b51d196bbe3caf3b3
Bruno David Martins <dbrunobruno369@gmail.com>
2026-01-21 23:45:19 -0300

    Acessando elementos de conjuntos

---
+++ acessandoelementos.py
@@ -0,0 +1,13 @@
+"""
+Para verificar a existencia de um item, utilizamos operadores in e not in
+"""
+
+#Considere
+conjunto = {1, 2, 3, 4, 5}
+print("Operador in: ")
+print(3 in conjunto)
+print(6 in conjunto)
+
+print("\nOperador not in")
+print(3 not in conjunto)
+print(6 not in conjunto)
--- conjuntos.py
+++ conjuntos.py
@@ -1,1 +1,22 @@
-a
+"""
+Um conjunto é uma coleção não ordenada de elementos únicos. Em Python, conjuntos são implementados usando a classe `set`. Eles suportam operações matemáticas como união, interseção e diferença.
+"""
+
+"""
+Chaves {} podem ser usadas para criar conjuntos diretamente, quando contiverem elementos. Use uma lista de elementos separados por vírgulas entre chaves. 
+"""
+conjunto1 = {'a', 'b', 'c'}
+
+""" 
+A função set() pode ser usada para criar conjuntos, especialmente quando precisamos inicializar um conjunto vazio.
+"""
+conjunto2 = set([3, 4, 5, 6])
+
+""" 
+Conjunto Vazio é uma coleção que não tem elementos
+"""
+
+tipo = type({})
+print(tipo) # Saída: <class 'dict'> - chaves vazias ciram outra estrutura, não um conjunto
+
+conjunto_vazio = set() # Para conjuntos vazios, use set() sem argumentos.
