10051b89fa581c8eeb0788eef1f17612aec33ded
Bruno David Martins <dbrunobruno369@gmail.com>
2026-01-21 23:52:48 -0300

    Metodos para conjuntos

--- metodos.py
+++ metodos.py
@@ -5,5 +5,9 @@ Conjuntos possuem diversos métodos para adicionar e remover elementos.
 #Considere
 conjunto = {1, 2, 3}
 
-conjunto.add(4)
-conjunto.update
+conjunto.add(4)            # Adiciona um único elemento. ex: add(elemento)
+conjunto.update([5,6,7])   # Adiciona vários elementos de uma vez. ex: update(iteráveis)
+conjunto.remove(2)         # Remove um item. Se o elemento não existir, gera um KeyError. ex: remove(elemento)
+conjunto.dicard(2)         # Remove um item. Se o elemento não existir, não gera erro. ex: discard(elemento)
+conjunto.pop()             # Remove e retorna um elemento. ex: pop()
+conjunto.clear()           # Remove todos os elementos do conjunto. ex: clear()
