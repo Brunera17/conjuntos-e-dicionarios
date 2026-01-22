8f7a4be3001e49c272d1d93175a777be9ff740be
Bruno David Martins <dbrunobruno369@gmail.com>
2026-01-22 00:48:22 -0300

    Dicionários

---
+++ dicionarios.py
@@ -0,0 +1,26 @@
+"""
+Dicionários são estruturas de dados que armazenam pares chave-valor, permitindo acesso rápido aos valores por meio das chaves, Eles servem para organizar dados de forma estruturada.
+Usamos chaves {} para criar dicionários diretamente, inserindo pares chave-valor separados por vírgulas, Cada par deve ter a chave e o valor separados por dois pontos (:).
+"""
+
+# Exemplo
+dic1 = {"Nome": "Ana", "Nota": 10.0}
+
+""" 
+A função dict() pode ser usada para criar dicionários, passando pares chave-valor como argumentos nomeados ou utilizando uma lista de tuplas.
+"""
+# Exemplo
+dic2 = dict(nome="Eva", nota=9.4)
+
+""" 
+Dicionários vazios
+
+Um dicionário vazio é um dicionário sem nenhum elemento.
+"""
+
+dicionario = {}
+dicionario = dict()
+
+""" 
+Um dicionário vazio pode ser criado usando {} ou dict(). Ele não contém nenhum par chave-valor inicialmente, mas pode ser preenchido conforme necessário.
+"""
