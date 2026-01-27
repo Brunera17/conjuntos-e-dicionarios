e38c1f653e80966b3e95832ddf70e3b19ffadd13
Bruno David Martins <dbrunobruno369@gmail.com>
2026-01-27 00:22:17 -0300

    Desafio 2

---
+++ desafios/2.py
@@ -0,0 +1,26 @@
+"""
+Clara é editora de uma revista e deseja comparar dois artigos para identificar quais palavras aparecem em ambos. Sua tarefa é criar um programa que receba dois textos e exiba o conjunto de palavras comuns entre eles.
+
+Exemplo de entrada:
+
+    Texto 1: O sol brilha forte no céu azul 
+
+    Texto 2: O céu azul anuncia um dia de sol intenso 
+
+Saída esperada:
+
+
+    Palavras em comum: {'o', 'azul', 'sol', 'céu'} 
+"""
+
+texto1 = set(input("Texto 1: ").lower().split())
+texto2 = set(input("Texto 2: ").lower().split())
+comum = set()
+if not texto1.isdisjoint(texto2):
+    for palavra in texto1:
+        if palavra in texto2:
+            print(palavra)
+# Ou
+
+comuns = texto1&texto2    # Pode ser usado 'texto1.intersection(texto2)'
+print(f"Palavras em comum: {comuns}")
