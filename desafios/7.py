58cf7b89ed5ade6f9f713cc8c5c29db69442ac8a
Bruno David Martins <dbrunobruno369@gmail.com>
2026-01-30 00:00:36 -0300

    desafio 7

--- desafios/7.py
+++ desafios/7.py
@@ -1,3 +1,33 @@
 """
+Ana percebeu que, após o cadastro inicial dos produtos, precisa atualizar a quantidade de um item específico no estoque. Sua tarefa é criar um programa que solicite o nome do produto e a nova quantidade, atualizando essa informação no dicionário de estoque.
 
+Exemplo de entrada:
+
+
+estoque = { 
+
+    "Caderno universitário": 50, 
+
+    "Caneta azul": 120, 
+
+    "Borracha branca": 30 
+
+} 
+Copiar código
+Nome do produto a ser atualizado: Caneta azul 
+
+Nova quantidade: 150 
+Copiar código
+Saída esperada:
+
+
+{ 
+
+    "Caderno universitário": 50, 
+
+    "Caneta azul": 150, 
+
+    "Borracha branca": 30 
+
+} 
 """
